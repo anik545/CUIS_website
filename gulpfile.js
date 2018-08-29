45ad6b5dc0584469a9a0cac6a98fd4160ba767b4
Anik Roy <anik545@gmail.com>
2018-08-29 18:53:46 +0100

    fix gulp copy, remove dist from git

--- gulpfile.js
+++ gulpfile.js
@@ -70,8 +70,13 @@ gulp.task('images', () => {
 // Clean output directory
 
 gulp.task('copy', () => {
-    return gulp.src(['./src/fonts/**/*'])
-        .pipe(gulp.dest('./dist/fonts'))
+    gulp.src(['./src/**/*', 
+            '!./src/*.html',
+            './src/*', '!./',
+            '!./src/css/**/*',
+            '!./src/js/**/*', 
+            '!./src/img/**/*'])
+        .pipe(gulp.dest('./dist'));
 
 });
 
@@ -85,14 +90,14 @@ gulp.task('default', ['clean'], () => {
         'images',
         'styles',
         'scripts',
-        'pages'
+        'pages',
     );
 });
 
-gulp.task('deploy', () => {
+gulp.task('deploy_prod', () => {
 
     // Dirs and Files to sync
-    const rsyncPaths = ['dist'];
+    const rsyncPaths = ['dist/'];
 
     // Default options for rsync
     const rsyncConf = {
@@ -108,8 +113,34 @@ gulp.task('deploy', () => {
 
     rsyncConf.hostname = 'shell.srcf.net'; // hostname
     rsyncConf.username = 'ar899'; // ssh username
-    rsyncConf.destination = 'indiasoc/out'; // path where uploaded files go
+    rsyncConf.destination = 'indiasoc/public_html/'; // path where uploaded files go
+    //TODO: add staging/production options
+    // Use gulp-rsync to sync the files
+    return gulp.src(rsyncPaths)
+        .pipe(rsync(rsyncConf));
+});
 
+gulp.task('deploy_staging', () => {
+
+    // Dirs and Files to sync
+    const rsyncPaths = ['dist/'];
+
+    // Default options for rsync
+    const rsyncConf = {
+        progress: true,
+        incremental: true,
+        relative: true,
+        emptyDirectories: true,
+        recursive: true,
+        clean: true,
+        chmod: 'ugo=rwX',
+        exclude: [],
+    };
+
+    rsyncConf.hostname = 'shell.srcf.net'; // hostname
+    rsyncConf.username = 'ar899'; // ssh username
+    rsyncConf.destination = 'public_html/'; // path where uploaded files go
+    //TODO: add staging/production options
     // Use gulp-rsync to sync the files
     return gulp.src(rsyncPaths)
         .pipe(rsync(rsyncConf));
