9fd65eb0aeb627dbca0d1860e27c7de3f1b0229a
Anik Roy <anik545@gmail.com>
2018-09-23 20:18:40 +0100

    add crop committee images task, htaccess

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ const runSequence = require('run-sequence');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
+const imageResize = require('gulp-image-resize');
 
 
 // Set the browser that you want to support
@@ -27,9 +28,11 @@ const AUTOPREFIXER_BROWSERS = [
 // Gulp task to minify CSS files
 gulp.task('styles', () => {
     return gulp.src('./src/css/**/*.css')
-    // Auto-prefix css styles for cross browser compatibility
+        // Auto-prefix css styles for cross browser compatibility
         .pipe(newer('./dist/css'))
-        .pipe(autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+        .pipe(autoprefixer({
+            browsers: AUTOPREFIXER_BROWSERS
+        }))
         // Minify the file
         .pipe(csso())
         // Output
@@ -39,7 +42,8 @@ gulp.task('styles', () => {
 // Gulp task to minify JavaScript files
 gulp.task('scripts', () => {
     return gulp.src('./src/js/**/*.js')
-    // Minify the file
+        // Minify the file
+        .pipe(newer('./dist/js'))
         .pipe(uglify())
         // Output
         .pipe(gulp.dest('./dist/js'))
@@ -48,6 +52,7 @@ gulp.task('scripts', () => {
 // Gulp task to minify HTML files
 gulp.task('pages', () => {
     return gulp.src(['./src/**/*.html'])
+        .pipe(newer('./dist/'))
         .pipe(htmlmin({
             collapseWhitespace: true,
             removeComments: true
@@ -65,24 +70,41 @@ gulp.task('images', () => {
         .pipe(gulp.dest('./dist/img'));
 });
 
-// Clean output directory
+gulp.task('crop-committee',  function() {
 
+    return gulp.src(['./src/img/committee/orig/*'])
+        .pipe(newer('./src/img/committee'))
+        .pipe(imageResize({
+            width: "720",
+            height: "720",
+            gravity: "North",
+            upscale: true,
+            crop: true,
+            cover: true
+        }))
+        .pipe(gulp.dest('./src/img/committee'));
+})
+
+// copy all the other files, e.g. .htaccess, cgi, etc.
 gulp.task('copy', () => {
-    gulp.src(['./src/**/*', 
+    gulp.src(['./src/**/*',
             '!./src/*.html',
-            './src/*', '!./',
+            './src/*',
+            './src/.*',
+            '!./',
             '!./src/css/**/*',
-            '!./src/js/**/*', 
-            '!./src/img/**/*'])
+            '!./src/js/**/*',
+            '!./src/img/**/*'
+        ])
         .pipe(gulp.dest('./dist'));
-
 });
 
+// Clean output directory
 gulp.task('clean', () => del(['dist']));
 
 
 // Gulp task to minify all files
-gulp.task('default', ['clean'], () => {
+gulp.task('default', () => {
     runSequence(
         'copy',
         'images',
